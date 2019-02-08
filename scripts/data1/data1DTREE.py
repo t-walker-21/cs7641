dfdbb53e7cdc651dd4396b43c9eceedc06c46aad
tevon <twalker81@gatech.edu>
2019-02-08 17:01:05 -0500

    move dataset1 into separate dir

---
+++ scripts/data1/data1DTREE.py
@@ -0,0 +1,33 @@
+import cv2
+from cleanData import Dataset1
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+
+data = Dataset1()
+
+x,y = data.fetch_data_multi()
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=6000)
+
+
+print len(X_train)
+print len(y_train)
+
+
+cls = tree.DecisionTreeClassifier(max_depth=20)
+
+cls.fit(X_train,y_train)
+tree.export_graphviz(cls,out_file='tree.dot')
+
+inf = cls.predict(X_test)
+gt = y_test.ravel()
+
+correct = 0
+
+for t in range(len(gt)):
+    if inf[t] == gt[t]:
+        correct += 1
+
+print correct / (len(gt) * 1.0)
