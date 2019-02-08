b24622da570ef11f699ca320f7a896ae0e7f2e63
tevon <tjw0018@uah.edu>
2019-02-07 22:31:09 -0500

    finished data cleaning script for grad admisssions and implemented SVM

--- scripts/data1SVM.py
+++ scripts/data1SVM.py
@@ -1,38 +1,41 @@
-import cv2
-from sklearn.datasets import fetch_mldata
 from sklearn.cross_validation import train_test_split
 from sklearn import svm
 import numpy as np
 import time
+from cleanData import Dataset1
 
-mnist = fetch_mldata('MNIST original')
+data = Dataset1()
 
-x = []
-y = []
- 
-for i in range(0,len(mnist.target)):
+x,y = data.fetch_data()
 
- x.append(mnist.data[i])
- y.append(int(mnist.target[i]))
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=200)
 
 
-
-x = np.asarray(x)
-y = np.asarray(y)
-
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.75, random_state=42)
+print len(X_train)
+print len(y_train)
 
 
-cls = svm.SVC(kernel='linear')
+
+#cls = svm.SVC(kernel='linear')
+#cls = svm.SVC(kernel='rbf')
+cls = svm.SVC(kernel='poly')
 print ("TRAINING MODEL")
-cls.fit(X_train,y_train)
+cls.fit(X_train,y_train.ravel())
 
 
-for t in range(0,len(y_test)):
+"""for t in range(0,len(y_test)):
  guess = []
  guess.append(X_test[t])
  guess = np.asarray(guess)
+ print cls.predict(guess), " ---> " , y_test[t]"""
 
- cv2.imshow(str(cls.predict(guess)[0]),X_test[t].reshape(28,28))
- cv2.waitKey(700)
+inf = cls.predict(X_test)
+gt = y_test.ravel()
+
+correct = 0
+
+for t in range(len(gt)):
+    if inf[t] == gt[t]:
+        correct += 1
+
+print correct / (len(gt) * 1.0)
---
+++ scripts/test.py
@@ -0,0 +1,7 @@
+from cleanData import Dataset1
+
+
+dat = Dataset1()
+
+x,y = dat.fetch_data()
+
