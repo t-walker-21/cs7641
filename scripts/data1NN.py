3470da963ff515c8234ec6eb89ce290c57796156
tevon <tjw0018@uah.edu>
2019-02-07 23:37:55 -0500

    NN implementation done

--- scripts/data1NN.py
+++ scripts/data1NN.py
@@ -5,48 +5,38 @@ from keras import applications
 from keras import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.optimizers import Adam, SGD
-from sklearn.datasets import fetch_mldata
-from keras.utils import to_categorical
-from sklearn.datasets import fetch_mldata
 from sklearn.cross_validation import train_test_split
 from sklearn import svm
-from numpy import argmax
+from cleanData import Dataset1
+from sklearn.metrics import confusion_matrix
+from keras.utils import to_categorical
 
-INIT_LR = 1E-4
-EPOCHS = 5
-BS = 32
+INIT_LR = 1E-3
+EPOCHS = 500
+BS = 10
 
-mnist = fetch_mldata('MNIST original')
+data = Dataset1()
 
-x = []
-y = []
- 
-for i in range(0,len(mnist.target)):
+x,y = data.fetch_data()
 
- x.append(mnist.data[i]) 
- y.append(mnist.target[i])
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=90)
+#y_train = to_categorical(y_train)
+#y_test = to_categorical(y_test)
 
-
-enc = to_categorical(y)
-
-x = np.asarray(x)
-y = np.asarray(enc)
-
-
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+print len(X_train)
+print len(y_train)
 
 
 cls = Sequential()
 
-cls.add(Dense(128,input_dim=784,activation='relu'))
-cls.add(Dense(64,activation='relu'))
+cls.add(Dense(7,input_dim=7,activation='sigmoid'))
+#cls.add(Dense(10,activation='sigmoid'))
 
-cls.add(Dense(10,activation='softmax'))
+cls.add(Dense(1,activation='sigmoid'))
 
 opt = Adam(lr=INIT_LR)
 
-cls.compile(loss="categorical_crossentropy", optimizer=opt,
+cls.compile(loss="binary_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
 
@@ -55,14 +45,18 @@ cls.compile(loss="categorical_crossentropy", optimizer=opt,
 
 print ("TRAINING MODEL")
 
-cls.fit(x,y,epochs=EPOCHS,batch_size=BS)
+cls.fit(X_train,y_train,epochs=EPOCHS,batch_size=BS)
 
 
+inf = cls.predict(X_test)
+gt = y_train.ravel()
+print inf
 
-for t in range(0,len(y_test)):
- guess = []
- guess.append(X_test[t])
- guess = np.asarray(guess)
+correct = 0
+"""
+for t in range(len(gt)):
+    if inf[t] == gt[t]:
+        correct += 1
 
- cv2.imshow(str(argmax(cls.predict(guess)[0])),X_test[t].reshape(28,28))
- cv2.waitKey(700)
+print correct
+print "score", correct / (len(gt) * 1.0)"""
