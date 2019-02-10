6d5011614f33bdae32e103e55dce57914be8d9a1
tevon <twalker81@gatech.edu>
2019-02-09 22:45:46 -0500

    dtree, cross validation implemented via SKLearn

--- scripts/data1/data1DTREE.py
+++ scripts/data1/data1DTREE.py
@@ -1,22 +1,29 @@
-import cv2
 from cleanData import Dataset1
 from sklearn import tree
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, ShuffleSplit
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 data = Dataset1()
 
 x,y = data.fetch_data_multi()
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=6000)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
 
 
 print len(X_train)
 print len(y_train)
 
 
-cls = tree.DecisionTreeClassifier(max_depth=2)
+
+cls = tree.DecisionTreeClassifier(max_depth=15)
+
+cv = ShuffleSplit(n_splits=5, test_size=0.2,random_state=0)
+print cross_val_score(cls,x,y,cv=cv)
+print np.mean(cross_val_score(cls,x,y,cv=cv))
+
+
+"""
 
 cls.fit(X_train,y_train)
 tree.export_graphviz(cls,out_file='tree.dot')
@@ -27,7 +34,32 @@ gt = y_test.ravel()
 correct = 0
 
 for t in range(len(gt)):
-    if inf[t] == gt[t]:
-        correct += 1
+        if inf[t] == gt[t]:
+                correct += 1
 
-print correct / (len(gt) * 1.0)
+acc = correct / (len(gt) * 1.0)
+print "test performance  ",acc
+
+
+
+depth_perf = []
+for depth in range (1,60):
+        cls = tree.DecisionTreeClassifier(max_depth=depth)
+
+        cls.fit(X_train,y_train)
+        tree.export_graphviz(cls,out_file='tree.dot')
+
+        inf = cls.predict(X_train)
+        gt = y_train.ravel()
+
+        correct = 0
+
+        for t in range(len(gt)):
+                if inf[t] == gt[t]:
+                        correct += 1
+        acc = correct / (len(gt) * 1.0)
+        print "test performance for depth: ", depth, ": " , acc
+        depth_perf.append(acc)
+
+plt.plot(depth_perf)
+plt.show()"""
