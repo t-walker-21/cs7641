65a50c0f473684b4d801bd8ed08d6ee80f3c21fc
tevon <twalker81@gatech.edu>
2019-02-09 23:38:18 -0500

    broke scripts into CV folders and dataset size

---
+++ scripts/data1/crossValidation/KNearestNeighbors.py
@@ -0,0 +1,66 @@
+from sklearn.model_selection import train_test_split, ShuffleSplit, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from cleanData import Dataset1
+import numpy as np
+import time
+
+data = Dataset1()
+
+x,y = data.fetch_data_multi()
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=412)
+
+
+print len(X_train)
+print len(y_train)
+
+
+#cls = KNeighborsClassifier(n_neighbors=1,metric="manhattan")
+cls = KNeighborsClassifier(n_neighbors=1,metric="manhattan")
+
+neighbor_perf = []
+for neigh in range (1,30):
+        cls = KNeighborsClassifier(n_neighbors=neigh,metric="manhattan")
+        cls2 = KNeighborsClassifier(n_neighbors=neigh)
+
+        cv = ShuffleSplit(n_splits=10, test_size=0.2,random_state=0)
+        score = np.mean(cross_val_score(cls,x,y,cv=cv))
+        score2 = np.mean(cross_val_score(cls2,x,y,cv=cv))
+        #print np.mean(cross_val_score(cls,x,y,cv=cv))
+        neighbor_perf.append([score,score2])
+
+plt.plot(neighbor_perf)
+plt.title('10-fold Cross-Validated Accuracy vs Neighbor Number')
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+
+"""
+
+cls.fit(X_train,y_train.ravel())
+
+
+inf = cls.predict(X_test)
+gt = y_test.ravel()
+
+correct = 0
+
+for t in range(len(gt)):
+    if inf[t] == gt[t]:
+        correct += 1
+
+print correct / (len(gt) * 1.0)
+
+#mat = confusion_matrix(cls.predict(X_test),y_test)
+
+
+#print(mat)
+
+plt.matshow(mat)
+plt.title('Confusion matrix')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()"""
---
+++ scripts/data1/crossValidation/cleanData.py
@@ -0,0 +1,68 @@
+import csv
+from sklearn import preprocessing
+import numpy as np
+THRESHOLD = 0.72
+
+
+
+class Dataset1:
+    def __init__(self):
+        pass
+
+
+
+    def fetch_data_bin(self):
+        X = []
+        Y = []
+        with open('../../../datasets/raw_data/data1.csv') as csvfile:
+            datareader = csv.reader(csvfile)
+
+            for row in datareader:
+                #print row
+                data = row[1:]
+                if float(data[-1]) >= THRESHOLD:
+                    data[-1] = 1
+                else:
+                    data[-1] = 0
+
+                
+                feats = data[:-1]
+                label = data[-1]
+                #print [float(i) for i in data]
+                X.append([float(i) for i in feats])
+                Y.append([float(label)])
+
+            X = np.array(X)
+            Y = np.array(Y)
+        
+
+            return X,Y
+
+    def fetch_data_multi(self):
+        X = []
+        Y = []
+        with open('../../../datasets/raw_data/data1.csv') as csvfile:
+            datareader = csv.reader(csvfile)
+
+            for row in datareader:
+                #print row
+                data = row[1:]
+                if float(data[-1]) <= 0.7:
+                    data[-1] = 0
+                elif float(data[-1]) <= 0.85:
+                    data[-1] = 1
+                else:
+                    data[-1] = 2
+
+            
+                feats = data[:-1]
+                label = data[-1]
+                #print [float(i) for i in data]
+                X.append([float(i) for i in feats])
+                Y.append([float(label)])
+
+        X = np.array(X)
+        Y = np.array(Y)
+    
+
+        return X,Y
