9ccfd3cc1f547bd14b97beddd6f936375de92986
tevon <twalker81@gatech.edu>
2019-02-10 00:01:42 -0500

    cross val and dataset size comps

---
+++ scripts/data1/crossValidation/neuralNetwork.py
@@ -0,0 +1,102 @@
+import numpy as np
+import os
+from keras import applications
+from keras import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.optimizers import Adam, SGD
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from cleanData import Dataset1
+from sklearn.metrics import confusion_matrix
+from keras.utils import to_categorical
+from matplotlib import pyplot as plt
+
+
+def get_highest(l):
+	high = max(l)
+	
+	for n in range(len(l)):
+		if l[n] == high:
+			l[n] = 1
+		else:
+			l[n] = 0
+
+	return l
+
+	
+
+
+INIT_LR = 1E-3
+EPOCHS = 900
+BS = 50
+seed = 9068
+
+np.random.seed(seed)
+
+data = Dataset1()
+
+x,y = data.fetch_data_multi()
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=seed)
+print y_train[3]
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+
+
+print len(X_train)
+print len(y_train)
+
+print y_train[1]
+
+
+cls = Sequential()
+
+cls.add(Dense(7,input_dim=7,activation='relu',kernel_initializer='random_uniform'))
+cls.add(Dense(100,activation='relu',kernel_initializer='random_uniform'))
+cls.add(Dense(3,activation='softmax',kernel_initializer='random_uniform'))
+
+opt = Adam(lr=INIT_LR)
+
+cls.compile(loss="categorical_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+
+
+
+
+print ("TRAINING MODEL")
+
+history = cls.fit(X_train,y_train,epochs=EPOCHS,steps_per_epoch=10,validation_split=0.2,validation_steps=50)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+
+inf = cls.predict(X_test)
+gt = y_test
+
+"""inf2 = []
+for num in inf:
+	if num >= 0.5:
+		inf2.append(1)
+	else:
+		inf2.append(0)
+inf2 = np.array(inf2)"""
+
+#print inf2
+
+correct = 0
+
+
+for t in range(len(gt)):
+    if (get_highest(inf[t]) == gt[t]).all():
+        correct += 1
+
+print correct
+print "score", correct / (len(gt) * 1.0)
---
+++ scripts/data1/datasetSize/KNearestNeighbors.py
@@ -0,0 +1,63 @@
+from sklearn.model_selection import train_test_split, ShuffleSplit, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from cleanData import Dataset1
+import numpy as np
+import time
+
+data = Dataset1()
+
+x,y = data.fetch_data_multi()
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=412)
+
+
+print len(X_train)
+print len(y_train)
+
+
+#cls = KNeighborsClassifier(n_neighbors=1,metric="manhattan")
+size_perf = []
+for data_size in np.arange(1,10,1):
+        cls = KNeighborsClassifier(n_neighbors=1,metric="manhattan")
+        size = 1 - (data_size) * 0.1
+        print "size: " , size
+        cv = ShuffleSplit(n_splits=1, test_size=size,random_state=102)
+        score = cross_val_score(cls,x,y,cv=cv)
+        #print np.mean(cross_val_score(cls,x,y,cv=cv))
+        size_perf.append(np.mean(score))
+
+plt.plot(size_perf)
+plt.title('Accuracy vs Data Train Size')
+plt.xlabel('Data Size (percent)')
+plt.ylabel('Accuracy')
+plt.show()
+
+"""
+
+cls.fit(X_train,y_train.ravel())
+
+
+inf = cls.predict(X_test)
+gt = y_test.ravel()
+
+correct = 0
+
+for t in range(len(gt)):
+    if inf[t] == gt[t]:
+        correct += 1
+
+print correct / (len(gt) * 1.0)
+
+#mat = confusion_matrix(cls.predict(X_test),y_test)
+
+
+#print(mat)
+
+plt.matshow(mat)
+plt.title('Confusion matrix')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()"""
