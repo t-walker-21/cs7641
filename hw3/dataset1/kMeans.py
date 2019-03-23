520f57372348b6aafd0718e7ba4b08e6490fe13e
tevon walker <tjw0018@uah.edu>
2019-03-23 15:20:32 -0400

    added elbow plot to code

--- hw3/dataset1/kMeans.py
+++ hw3/dataset1/kMeans.py
@@ -1,4 +1,9 @@
 from sklearn.cluster import KMeans
+import numpy as np
+from sklearn import metrics
+from scipy.spatial.distance import cdist
+import matplotlib.pyplot as plt
+
 fin = open("data1.txt","r")
 
 
@@ -9,14 +14,22 @@ for l in fin:
     X.append(l.split(",")[:-2])
     y.append(int(l.split(",")[-2]))
 
+X = np.array(X,dtype=np.float32)
 
-#print len(X)
-#print len(y)
-
-
-
-km = KMeans(n_clusters=3,random_state=0).fit(X[:-100])
+# determine k using elbow method
 
 
-print km.predict(X[0:2])
-print y[0:2]
+# k means determine k
+distortions = []
+K = range(1,10)
+for k in K:
+    kmeanModel = KMeans(n_clusters=k).fit(X)
+    kmeanModel.fit(X)
+    distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+
+# Plot the elbow
+plt.plot(K, distortions, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Distortion')
+plt.title('The Elbow Method showing the optimal k')
+plt.show()
