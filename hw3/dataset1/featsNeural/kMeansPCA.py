1cd72906f2122fe0818d3220fbe4ef3ee9c9db66
tevon walker <tjw0018@uah.edu>
2019-03-24 15:09:53 -0400

    added clust feats for nerual net

--- hw3/dataset1/featsNeural/kMeansPCA.py
+++ hw3/dataset1/featsNeural/kMeansPCA.py
@@ -1,9 +1,10 @@
-from sklearn.decomposition import PCA
-fin = open("../data1.txt","r")
+from sklearn.cluster import KMeans
 import numpy as np
-import matplotlib.pyplot as plt
+from sklearn import metrics
+from scipy.spatial.distance import cdist
 from sklearn.preprocessing import StandardScaler
-import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 import os
 from keras import applications
 from keras import Sequential
@@ -14,6 +15,27 @@ from sklearn.metrics import confusion_matrix
 from matplotlib import pyplot as plt
 from keras.utils import to_categorical
 
+
+def plot_elbow(X):
+    distortions = []
+    K = range(1,10)
+    for k in K:
+        kmeanModel = KMeans(n_clusters=k).fit(X)
+        kmeanModel.fit(X)
+        distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+
+    # Plot the elbow
+    plt.plot(K, distortions, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Distortion')
+    plt.title('The Elbow Method showing the optimal k')
+    plt.show()
+
+
+
+fin = open("../data1.txt","r")
+
+
 X = []
 y = []
 
@@ -22,6 +44,7 @@ for l in fin:
     y.append(int(l.split(",")[-2]))
 
 X = np.array(X,dtype=np.float32)
+
 scaler = StandardScaler()
 scaler.fit(X)
 
@@ -29,6 +52,23 @@ X = scaler.transform(X)
 pca = PCA(n_components=2)
 pca.fit(X)
 dr_X = pca.transform(X)
+
+#obtain elbow plot
+#plot_elbow(dr_X)
+
+#pick three clusters, and view a few groupings
+
+km = KMeans(n_clusters=2,random_state=0).fit(dr_X)
+
+newX = []
+
+for pt in dr_X:
+    newX.append(km.predict(pt.reshape(1,-1))[0])
+
+newX = np.array(newX)
+
+newX = to_categorical(newX)
+y = to_categorical(y)
 
 
 INIT_LR = 5E-4
@@ -38,8 +78,8 @@ seed = 9068
 
 np.random.seed(seed)
 
+print newX[0]
 
-y = to_categorical(y)
 
 cls = Sequential()
 
@@ -51,7 +91,7 @@ opt = Adam(lr=INIT_LR)
 
 cls.compile(loss="categorical_crossentropy", optimizer=opt,metrics=["accuracy"])
 
-history = cls.fit(dr_X,y,epochs=EPOCHS,steps_per_epoch=10,validation_split=0.2,validation_steps=50)
+history = cls.fit(newX,y,epochs=EPOCHS,steps_per_epoch=10,validation_split=0.2,validation_steps=50)
 
 
 plt.plot(history.history['loss'])
