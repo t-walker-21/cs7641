a2427094079f09dc7b7dbab07c2621b4770d3a56
tevon walker <tjw0018@uah.edu>
2019-03-24 08:54:33 -0400

    component analysis

--- hw3/dataset1/ica.py
+++ hw3/dataset1/ica.py
@@ -1,4 +1,7 @@
-from sklearn.decomposition import FastICA
+from sklearn.decomposition import FastICA as ICA
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
 fin = open("data1.txt","r")
 import numpy as np
 
@@ -9,7 +12,33 @@ for l in fin:
     X.append(l.split(",")[:-2])
     y.append(int(l.split(",")[-2]))
 
-pca = PCA(n_components=3)
-pca.fit(X)
+X = np.array(X,dtype=np.float32)
+scaler = StandardScaler()
+scaler.fit(X)
 
-print (pca.explained_variance_ratio_)
+X = scaler.transform(X)
+
+ica = ICA(n_components=2)
+ica.fit(X)
+
+X = ica.transform(X)
+
+newData = [[],[],[]]
+i = 0
+
+for pt in X:
+    newData[y[i]].append(pt)
+    i += 1
+
+
+print np.array(newData[0]).shape
+for i in range(len(newData[0])):
+    plt.scatter(newData[0][i][0],newData[0][i][1],marker='+',color='r')
+    
+for i in range(len(newData[1])):
+    plt.scatter(newData[1][i][0],newData[1][i][1],marker='+',color='g')
+
+for i in range(len(newData[2])):
+    plt.scatter(newData[2][i][0],newData[2][i][1],marker='+',color='b')
+
+plt.show()
